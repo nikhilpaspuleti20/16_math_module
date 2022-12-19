7c07f0c0a64d88792fd60e78933c7e6cebb572b3
nikhilpaspuleti20 <116423567+nikhilpaspuleti20@users.noreply.github.com>
2022-12-19 20:37:17 +0530

    Add files via upload

---
+++ maths_module.py
@@ -0,0 +1,13 @@
+'''
+DATE:12TH NOV 2022
+DAY: MONDAY
+DATE:13TH DEC 2022
+DAY: TUESDAY
+TOPIC: MATH MODULE
+AUTHOR: nikhil
+'''
+print(math.sqrt(6)) #2.44948974278318
+print(math.sqrt(25)) #5.0
+n=[1,3,5]
+print(math.fsum(n)) #9.0
+print(math.fsum(n)) #9.0
